7ef6742e381a2a79b9b536377520be665d57adfc
青萝菜 瓜冬卜 <i@chainsx.cn>
2025-11-13 11:24:55 +0800

    Create zero2w-config.mk

---
+++ zero2w-config.mk
@@ -0,0 +1,23 @@
+# Base board
+BOARD = zero2w
+
+# Hardware configuration
+PLATFORM = v2-hdmi
+
+# Target hostname
+HOSTNAME = pikvm
+
+# ru_RU, etc. UTF-8 only
+LOCALE = en_US
+
+# See /usr/share/zoneinfo
+TIMEZONE = Asia/Shanghai
+
+# For SSH root user
+ROOT_PASSWD = root
+
+# Web UI credentials: user=admin, password=adminpass
+WEBUI_ADMIN_PASSWD = admin
+
+# IPMI credentials: user=admin, password=adminpass
+IPMI_ADMIN_PASSWD = admin
